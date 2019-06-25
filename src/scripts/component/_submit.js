ce3aa6d2246d76fc5fc14b01edd9cd7e4d534db3
yoshida-hiro <yoshida-hiro@gnavi.co.jp>
2019-06-25 14:44:33 +0900

    途中

--- src/scripts/component/_submit.js
+++ src/scripts/component/_submit.js
@@ -1,7 +1,10 @@
 export const submit = () => {
   const $input = document.querySelector('.js-input')
   const $submitText = document.querySelector('.js-addtext')
+  //const data = $input.value
   const data = $input.value
 
-  $submitText.insertAdjacentHTML('afterbegin',data)
+  $submitText.innerHTML = ''
+  $submitText.classList.add('is-active')
+  $submitText.insertAdjacentHTML('afterbegin', data)
 }
